be5b3ff0e88f56b1346ceaa4fc567da9dbf854b5
ChepikPolina <pichepik@gmail.com>
2018-04-17 13:51:00 +0300

    added documentation

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Build Status](https://api.travis-ci.org/ChepikPolina/lab07.svg?branch=master)](https://travis-ci.org/ChepikPolina/lab07)
--- include/print.hpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <fstream>
-#include <iostream>
-//comment for print function
-void print(const std::string& text, std::ostream& out = std::cout);
-void print(const std::string& text, std::ofstream& out);
